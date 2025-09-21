6f857a259332064c0ce7e9e2afd6dce6f8d2f28d
Sythv <Redduzyt@gmail.com>
2025-09-21 00:35:50 -0500

    Deleted all comments in Main.php

--- Replenish/src/Sythv/Main.php
+++ Replenish/src/Sythv/Main.php
@@ -11,18 +11,13 @@ class Main extends PluginBase
 {
     protected function onEnable(): void
     {
-        /** This shows you that the plugin is working in the console */
         $this->getLogger()->info("enable");
-        /** This is to register the initCommands function */
         $this->initCommands();
     }
-
-    /** Place all your command class names here, so they get registered. */
     public function initCommands(): void
     {
         $commands = [
             new Replenish(),
-            /** Add more commands here as needed.*/
         ];
         $this->getServer()->getCommandMap()->registerAll("Sythv" ,$commands);
     }
